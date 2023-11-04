ab49357ed0671c1d533305c0bd198f79ddd8964f
Mustakim <m.m.kiron@gmail.com>
2023-11-04 19:20:30 +0600

    Fix linter errors

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,28 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-    before :all do
-      @user = User.create(name: 'John', posts_counter: 0)
-      @post = Post.create(author: @user, title: 'Title', comments_counter: 0, likes_counter: 0)
+  before :all do
+    @user = User.create(name: 'John', posts_counter: 0)
+    @post = Post.create(author: @user, title: 'Title', comments_counter: 0, likes_counter: 0)
+  end
+
+  context 'Add like' do
+    it 'is not valid without user' do
+      expect(Like.new(post: @post)).to_not be_valid
     end
-  
-    context 'Add like' do
-      it 'is not valid without user' do
-        expect(Like.new(post: @post)).to_not be_valid
-      end
-  
-      it 'is not valid without post' do
-        expect(Like.new(user: @user)).to_not be_valid
-      end
-    end
-  
-    context 'update_likes_counter' do
-      before :all do
-        8.times { Like.create(user: @user, post: @post)}
-      end
-  
-      it 'keeps track of Likes and equals 8' do
-        expect(@post.likes_counter).to eq 8
-      end
+
+    it 'is not valid without post' do
+      expect(Like.new(user: @user)).to_not be_valid
     end
   end
+
+  context 'update_likes_counter' do
+    before :all do
+      8.times { Like.create(user: @user, post: @post) }
+    end
+
+    it 'keeps track of Likes and equals 8' do
+      expect(@post.likes_counter).to eq 8
+    end
+  end
+end

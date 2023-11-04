de477c53d7f8aa1eb8bdd66dc59e339f04779602
Mustakim <m.m.kiron@gmail.com>
2023-11-04 19:08:19 +0600

    Add unit test for method in Comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -15,4 +15,14 @@ RSpec.describe Comment, type: :model do
       expect(Comment.new(user: @user)).to_not be_valid
     end
   end
+
+  context '#update_comments_counter' do
+    before :all do
+      8.times { |comment_i| Comment.create(user: @user, post: @post, text: (comment_i + 1).to_s) }
+    end
+
+    it 'keeps track of comments and equals 8' do
+      expect(@post.comments_counter).to eq 8
+    end
+  end
 end

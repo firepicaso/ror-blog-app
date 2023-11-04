a072ccc489ed39a843f2f2dd3b691a488679c448
Mustakim <m.m.kiron@gmail.com>
2023-11-04 11:19:51 +0600

    Add name validation to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :comment
   has_many :like
 
+  validates :name, presence: true
+
   def three_recent_posts
     Post.where(author: self).order(created_at: :desc).first(3)
   end

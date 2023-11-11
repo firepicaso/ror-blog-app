0ae1bbc2bc102a59aca6a01345f5d986d81656ba
Mustakim <m.m.kiron@gmail.com>
2023-11-11 20:08:40 +0600

    Add routes for create post and comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.build(comment_params.merge(post: @post))
+
+    if @comment.save
+      redirect_to user_post_path(@post.author, @post), notice: 'Comment was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end

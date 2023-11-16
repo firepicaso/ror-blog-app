a0aa37254b31e38af0cbeb37ee462b2868efcfce
Mustakim <m.m.kiron@gmail.com>
2023-11-16 11:43:07 +0600

    Fix posts_counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,8 +10,6 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  after_create :update_posts_counter
-
   def five_recent_comments
     Comment.where(post_id: id).order(created_at: :desc).limit(5)
   end

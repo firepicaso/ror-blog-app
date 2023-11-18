5a4f10c4f8ee2e3bda9d1e7ee02a284cba8b4663
Mustakim <m.m.kiron@gmail.com>
2023-11-18 13:45:36 +0600

    Create rswag API documentation

--- spec/integration/api/blogs_spec.rb
+++ spec/integration/api/blogs_spec.rb
@@ -1,4 +1,29 @@
+# spec/requests/blogs_spec.rb
 require 'swagger_helper'
 
-RSpec.describe 'api/blogs', type: :request do
+describe 'RoR Blog App API' do
+  path '/users/1/posts/' do
+
+    get 'Retrieves posts for a specific user' do
+      tags 'Posts'
+      produces 'application/json'
+
+      response '200', 'posts found' do
+        schema type: :array,
+          items: {
+            type: :object,
+            properties: {
+              title: { type: :string },
+            },
+          },
+          required: ['title']          
+        run_test!
+      end
+
+      response '404', 'user not found' do
+        let(:user_id) { 999 } # Assuming 999 is an invalid user_id
+        run_test!
+      end
+    end
+  end
 end
